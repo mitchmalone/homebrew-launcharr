e20e7faafeecf3fe0c0c22c7c0e4334012ad89e6
Mitch Malone <mitch@mitchmalone.com>
2026-08-10 13:08:03 +1000

    launcharr v0.3.0

--- Casks/launcharr.rb
+++ Casks/launcharr.rb
@@ -1,6 +1,6 @@
 cask "launcharr" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "0.3.0"
+  sha256 "bb49811ea95c11f9316f90c0603f6b5f7cb5547680b5ecb80ddd6b51cb9952e0"
 
   url "https://github.com/mitchmalone/launcharr/releases/download/v#{version}/launcharr-#{version}.zip"
   name "launcharr"
